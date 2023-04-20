54d789023c64df545be1f6b203b5dbab81b0d781
kusseba <131396062+kusseba@users.noreply.github.com>
2023-04-20 16:14:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 </head>
 <body>
   <h1>Bienvenidos Hola Mundo</h1>
+  <p> Prueba para ver si se realizan cambios en el código inicial<p>
   <h2>ChoicesPracticas Profesionalizantes</2>
+  <p> Exito en la primara prueba confirmado<p>
+  <h2>Las pruebas resultan en exito. Al menos dentro de los registros del Git Hud.<h2>
 </body>
 </html>
